f092ca77a8c8550891a43ae7d2b895c46b280b84
Chopa1 <maltzevvadim48@gmail.com>
2024-03-08 20:31:01 +0700

    border commit

--- src/components/icons/IconArrowTheme.vue
+++ src/components/icons/IconArrowTheme.vue
@@ -3,3 +3,10 @@
         <path d="M13.75 11.975L8.79999 7.02501L9.87499 5.95001L15.9 11.975L9.87499 18L8.79999 16.925L13.75 11.975Z" fill="#8593AB"/>
     </svg>
 </template>
+
+<style scoped>
+    svg {
+        border-radius: 15px;
+        background-color: rgba(233, 242, 255, 0.4);
+    }
+</style>
